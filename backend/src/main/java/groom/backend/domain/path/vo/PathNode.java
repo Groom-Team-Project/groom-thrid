dcf000c4e64dcb57328b703398a6b08fd557ea00
kmgyu <lidersy961@gmail.com>
2025-11-27 20:52:28 +0900

    feat(path): VO 정의

---
+++ backend/src/main/java/groom/backend/domain/path/vo/PathNode.java
@@ -0,0 +1,20 @@
+package groom.backend.domain.path.vo;
+
+import java.util.List;
+
+/**
+ * LineString 또는 Point 타입의 Feature를 저장
+ * TMAP API version 1에 맞춰 properties가 들어감.
+ */
+public class PathNode {
+  private String type;
+  private Integer index;
+  private List<Double> coordinates; // 경도, 위도 순
+  private String name;
+  private String description;
+  private Integer distance; // 거리 단위시간 미터
+  private Integer roadType;
+  private Integer time; // 소요시간 단위시간 초
+  private Integer categoryRoadType;
+  private Integer facilityType;
+}
---
+++ backend/src/main/java/groom/backend/domain/path/vo/PathSummary.java
@@ -0,0 +1,12 @@
+package groom.backend.domain.path.vo;
+
+/**
+ * 경로 요약 정보
+ * totalDistance : 거리, 단위길이 미터
+ * totalTime : 소요시간, 단위시간 분
+ * 시작점, 종료점
+ */
+public class PathSummary {
+  private Integer totalDistance;
+  private Integer totalTime;
+}
