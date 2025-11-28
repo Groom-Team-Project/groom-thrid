16db7d3442758846e857fc9b0c566d0e38650a28
kmgyu <lidersy961@gmail.com>
2025-11-28 23:32:50 +0900

    feat(path): TMAP API Client 추가 - API 통신을 위한 Rest Client 추가

---
+++ backend/src/main/java/groom/backend/domain/path/dto/request/TmapApiRequest.java
@@ -0,0 +1,23 @@
+package groom.backend.domain.path.dto.request;
+
+import lombok.*;
+
+// tmap 요청을 위한 백엔드 측의 DTO
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TmapApiRequest {
+  private Double startX;
+  private Double startY;
+  private Double endX;
+  private Double endY;
+  private String startName;
+  private String endName;
+  @Builder.Default
+  private Integer searchOption = 30; // 경로 옵션 : 계단 회피
+  @Builder.Default
+  private String sort = "index"; // 반환 시 경로 순서대로.
+}
---
+++ backend/src/main/java/groom/backend/domain/path/service/spec/TmapApiClient.java
@@ -0,0 +1,37 @@
+package groom.backend.domain.path.service.spec;
+
+import groom.backend.domain.path.dto.request.PathFindRequest;
+import groom.backend.domain.path.dto.response.PathFindResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+@Slf4j
+public class TmapApiClient {
+  private final String tmapApiKey;
+
+  private final RestClient restClient;
+
+  public TmapApiClient(RestClient.Builder builder,
+                  @Value("${api.tmap.url}") String tmapUrl,
+                  @Value("${api.tmap.api-key}") String tmapApiKey) {
+    this.restClient = builder.baseUrl(tmapUrl).build();
+    this.tmapApiKey = tmapApiKey;
+  }
+
+  public PathFindResponse tmapApiPathFind(PathFindRequest pathFindRequest) {
+    Integer version = 1;
+    log.info(this.tmapApiKey);
+    PathFindResponse pathFindResponse = restClient.post().uri(uriBuilder -> uriBuilder
+            .path("/path")
+            .queryParam("version", version).build())
+            .header("appKey", tmapApiKey)
+            .header("Accept", "*/*")
+            .header("Content-Type", "application/json")
+            .body(pathFindRequest).retrieve().body(PathFindResponse.class);
+
+    return pathFindResponse;
+  }
+}
