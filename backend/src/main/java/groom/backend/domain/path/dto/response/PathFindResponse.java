878ca458c1cf176ff92e9fe08a35590860fc4506
kmgyu <lidersy961@gmail.com>
2025-11-29 22:18:26 +0900

    feat(path): 경로 반환 DTO 필드 설정

--- backend/src/main/java/groom/backend/domain/path/dto/response/PathFindResponse.java
+++ backend/src/main/java/groom/backend/domain/path/dto/response/PathFindResponse.java
@@ -1,6 +1,11 @@
 package groom.backend.domain.path.dto.response;
 
+import groom.backend.domain.path.vo.PathNode;
+import groom.backend.domain.path.vo.PathSummary;
+
+import java.util.List;
+
 public class PathFindResponse {
-  // PathSummaryVO
-  // List<PathNodeVO>
+   private PathSummary pathSummary;
+   private List<PathNode> pathNodeList;
 }
