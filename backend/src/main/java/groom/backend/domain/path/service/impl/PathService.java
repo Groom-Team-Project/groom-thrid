d101eef257b42ce7f78ba5fee726879f809bb267
kmgyu <lidersy961@gmail.com>
2025-11-30 23:56:01 +0900

    feat(path): 길찾기 Service 구현체 정의

--- backend/src/main/java/groom/backend/domain/path/service/impl/PathService.java
+++ backend/src/main/java/groom/backend/domain/path/service/impl/PathService.java
@@ -1,15 +1,7 @@
 package groom.backend.domain.path.service.impl;
 
+import groom.backend.domain.path.dto.response.PathFindResponse;
+
 public interface PathService {
-  private void findPath() {
-    // 서비스 제공 구역 검사
-
-    // 제공 구역이 아닐 경우, Exception 발생
-
-    // API 호출, 값 구해오기
-
-    // DTO로 직렬화
-
-    // 반환
-  }
+  public PathFindResponse findPath();
 }
