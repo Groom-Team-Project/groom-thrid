912401c19bce8149fe624168ed5ce0b9dbb2a9d0
kmgyu <lidersy961@gmail.com>
2025-12-01 18:53:08 +0900

    feat(path): Kakao 위경도 주소변환 API 추가

---
+++ backend/src/main/java/groom/backend/interfaces/kakao/KakaoApiClient.java
@@ -0,0 +1,38 @@
+package groom.backend.interfaces.kakao;
+
+import groom.backend.interfaces.kakao.dto.request.KakaoAddressRequest;
+import groom.backend.interfaces.kakao.dto.response.KakaoAddressResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+@Slf4j
+public class KakaoApiClient {
+  private final String kakaoApiKey;
+
+  private final RestClient restClient;
+
+  public KakaoApiClient(RestClient.Builder builder,
+                       @Value("${api.kakao.url}") String kakaoUrl,
+                       @Value("${api.kakao.rest-api-key}") String kakaoApiKey) {
+    this.restClient = builder.baseUrl(kakaoUrl).build();
+    this.kakaoApiKey = kakaoApiKey;
+  }
+
+  public KakaoAddressResponse transferToAddress(KakaoAddressRequest request) {
+    log.info("Kakao AK " + kakaoApiKey);
+    KakaoAddressResponse response = restClient.get().uri(uriBuilder -> uriBuilder
+                    .path("/v2/local/geo/coord2address.json")
+                    .queryParam("x", request.getX())
+                    .queryParam("y", request.getY())
+                    .queryParam("input_coord", request.getInputCoord())
+                    .build())
+            .header("Authorization", "Kakao AK " + kakaoApiKey)
+            .header("Content-Type", "application/json;charset=UTF-8")
+
+            .retrieve().body(KakaoAddressResponse.class);
+    return response;
+  }
+}
