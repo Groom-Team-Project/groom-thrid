dbcdeb00540d02d1eb1144541b83a53b7bbb93da
kmgyu <lidersy961@gmail.com>
2025-12-01 10:45:52 +0900

    Revert "feat(path): 길찾기 Service 구현체 정의"
    This reverts commit d101eef257b42ce7f78ba5fee726879f809bb267.

--- backend/src/main/java/groom/backend/domain/location/repository/impl/LocationRespositoryImpl.java
+++ backend/src/main/java/groom/backend/domain/location/repository/impl/LocationRespositoryImpl.java
@@ -1,17 +1,6 @@
 package groom.backend.domain.location.repository.impl;
 
-import groom.backend.domain.location.dto.reponse.CreateLocationResponse;
-import groom.backend.domain.location.dto.request.CreateLocationRequest;
 import groom.backend.domain.location.service.spec.LocationService;
 
 public class LocationRespositoryImpl implements LocationService {
-  @Override
-  public CreateLocationResponse createLocation(CreateLocationRequest request) {
-    return null;
-  }
-
-  @Override
-  public CreateLocationResponse getLocation(Long locationId) {
-    return null;
-  }
 }
--- backend/src/main/java/groom/backend/domain/path/service/impl/PathService.java
+++ backend/src/main/java/groom/backend/domain/path/service/impl/PathService.java
@@ -1,7 +1,15 @@
 package groom.backend.domain.path.service.impl;
 
-import groom.backend.domain.path.dto.response.PathFindResponse;
-
 public interface PathService {
-  public PathFindResponse findPath();
+  private void findPath() {
+    // 서비스 제공 구역 검사
+
+    // 제공 구역이 아닐 경우, Exception 발생
+
+    // API 호출, 값 구해오기
+
+    // DTO로 직렬화
+
+    // 반환
+  }
 }
