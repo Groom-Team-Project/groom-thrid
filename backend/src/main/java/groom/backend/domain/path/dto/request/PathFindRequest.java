93c80cc642f716514d26b61e855882b35de11859
kmgyu <lidersy961@gmail.com>
2025-12-02 11:31:14 +0900

    feat(path): 길찾기 DTO validation 추가

--- backend/src/main/java/groom/backend/domain/path/dto/request/PathFindRequest.java
+++ backend/src/main/java/groom/backend/domain/path/dto/request/PathFindRequest.java
@@ -1,5 +1,7 @@
 package groom.backend.domain.path.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -8,10 +10,16 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PathFindRequest {
+  @NotNull
   private Double startX;
+  @NotNull
   private Double startY;
+  @NotNull
   private Double endX;
+  @NotNull
   private Double endY;
+  @NotBlank
   private String startName;
+  @NotBlank
   private String endName;
 }
