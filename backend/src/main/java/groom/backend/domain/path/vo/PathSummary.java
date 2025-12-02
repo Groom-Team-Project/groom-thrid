ee5970519f5bf38918b2c9b1a4a7946022ca2769
kmgyu <lidersy961@gmail.com>
2025-12-02 14:54:08 +0900

    bug(path): Path VO 어노테이션 추가 및 필드 수정과 Mapper 수정 - DTO 좌표 정보 필드 수정 - DTO 생성자 및 Getter, Setter 추가 - (chore) DTO 주석 추가 - Mapper 패턴에 현재 수정된 DTO 적용

--- backend/src/main/java/groom/backend/domain/path/vo/PathSummary.java
+++ backend/src/main/java/groom/backend/domain/path/vo/PathSummary.java
@@ -1,11 +1,18 @@
 package groom.backend.domain.path.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 경로 요약 정보
  * totalDistance : 거리, 단위길이 미터
  * totalTime : 소요시간, 단위시간 분
  * 시작점, 종료점
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PathSummary {
   private Integer totalDistance;
   private Integer totalTime;
--- backend/src/main/java/groom/backend/interfaces/tmap/mapper/TmapToPathMapper.java
+++ backend/src/main/java/groom/backend/interfaces/tmap/mapper/TmapToPathMapper.java
@@ -2,12 +2,58 @@ package groom.backend.interfaces.tmap.mapper;
 
 import groom.backend.domain.path.dto.request.PathFindRequest;
 import groom.backend.domain.path.dto.response.PathFindResponse;
+import groom.backend.domain.path.vo.PathNode;
+import groom.backend.domain.path.vo.PathSummary;
 import groom.backend.interfaces.tmap.dto.request.TmapPathFindRequest;
 import groom.backend.interfaces.tmap.dto.response.TmapPathFindResponse;
+import groom.backend.interfaces.tmap.dto.response.TmapPathGeometry;
+import groom.backend.interfaces.tmap.dto.response.TmapPathNodeFeature;
+import groom.backend.interfaces.tmap.dto.response.TmapPathNodeProperties;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 경로 찾기 API와 Tmap API DTO 호환을 위한 Data Mapper
+ */
 public class TmapToPathMapper {
   public static PathFindResponse toPathFindResponseDto(TmapPathFindResponse tmapPathFindResponse) {
-    return new PathFindResponse(tmapPathFindResponse.getPathSummary(), tmapPathFindResponse.getPathNodeList());
+    // 첫번째 노드에 요약정보 저장됨
+    TmapPathNodeProperties firstProperties = tmapPathFindResponse.getPathNodeList().getFirst().getProperties();
+
+    PathSummary pathSummary = new PathSummary(firstProperties.getTotalDistance(), firstProperties.getTotalDistance());
+//    .forEach((pathNode) -> {pathNode.})
+    List<PathNode> pathNodeList = new ArrayList<>();
+    for( TmapPathNodeFeature node : tmapPathFindResponse.getPathNodeList() ) {
+      TmapPathGeometry geometry = node.getGeometry(); // 위치 정보
+      TmapPathNodeProperties properties = node.getProperties(); // 부가 정보
+
+      // 좌표 추출
+      List<List<Double>> coordinates = new ArrayList<>();
+      String type = geometry.getType();
+      if (type.equals("Point")) {
+        coordinates.add((List<Double>) geometry.getCoordinates());
+      }
+      else if (type.equals("LineString")) {
+        coordinates = (List<List<Double>>) geometry.getCoordinates();
+      }
+
+      PathNode pathNode = new PathNode(
+              geometry.getType(),
+              properties.getIndex(),
+              coordinates,
+              properties.getName(),
+              properties.getDescription(),
+              properties.getDistance(),
+              properties.getRoadType(),
+              properties.getTime(),
+              properties.getCategoryRoadType(),
+              (Integer) Integer.parseInt(properties.getFacilityType() != "" ?
+                      properties.getFacilityType() : "1") // facility Type이 문자열로 직렬화되기 때문에 정수형변환 필요, 종료지점의 경우 empty 반환함.
+      );
+      pathNodeList.add(pathNode);
+    }
+    return new PathFindResponse(pathSummary, pathNodeList);
   }
 
   public static TmapPathFindRequest toTmapPathFindRequestDto(PathFindRequest PathFindRequest) {
