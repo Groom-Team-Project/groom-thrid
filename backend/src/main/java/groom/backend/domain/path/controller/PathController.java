7fb45ff4606ad8712816125d57608dc100334bc8
kmgyu <lidersy961@gmail.com>
2025-12-02 11:30:41 +0900

    feat(path): 길찾기 REST API 및 로직 구현

--- backend/src/main/java/groom/backend/domain/path/controller/PathController.java
+++ backend/src/main/java/groom/backend/domain/path/controller/PathController.java
@@ -1,14 +1,26 @@
 package groom.backend.domain.path.controller;
 
+import groom.backend.domain.path.dto.request.PathFindRequest;
+import groom.backend.domain.path.dto.response.PathFindResponse;
+import groom.backend.domain.path.service.impl.PathService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/paths")
 public class PathController {
+  private final PathService pathService;
 
-  public void pathFind() {
-    // 입력 유효성 검사 (필드에서 검증)
+  @GetMapping
+  public PathFindResponse pathFind(@Valid @RequestBody PathFindRequest pathFindRequest) {
+    // 입력 DTO 유효성 검사 (필드에서 검증)
 
-    // 서비스 레이어 findPath() 호출
+    return pathService.findPath(pathFindRequest);
   }
 
 }
--- backend/src/main/java/groom/backend/domain/path/service/spec/PathServiceImpl.java
+++ backend/src/main/java/groom/backend/domain/path/service/spec/PathServiceImpl.java
@@ -9,27 +9,35 @@ import groom.backend.domain.path.service.impl.PathService;
 import groom.backend.interfaces.kakao.KakaoApiClient;
 import groom.backend.interfaces.kakao.mapper.KakaoAddressMapper;
 import groom.backend.interfaces.tmap.TmapApiClient;
+import groom.backend.interfaces.tmap.mapper.TmapToPathMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PathServiceImpl implements PathService {
   private final TmapApiClient tmapApiClient;
   private final KakaoApiClient kakaoApiClient;
 
   @Override
   public PathFindResponse findPath(PathFindRequest pathFindRequest)  {
-    // 서비스 제공 구역 검사 (시작점, 종료점 각각 검사)
-
-    // 제공 구역이 아닐 경우, Exception 발생
+    // 서비스 제공 구역인지 검사 (시작점, 종료점 각각 검사)
+    // 서비스 제공 구역 여부를 검사하는 것이므로, 두 검증에서 모두 false가 나와야 한다. 시도 단위로 제공 시 시군구 단위 정보를 제공하지 않음.
+    if(!isProvisionArea(pathFindRequest.getStartX(), pathFindRequest.getStartY()) &&
+            !isProvisionArea(pathFindRequest.getEndX(), pathFindRequest.getEndY()) ) {
+      // TODO : 제공 구역이 아닐 경우, 대안 사용. Kakao URL scheme 반환
+      log.info("서비스 미제공 구역");
+      return null;
+    }
 
     // API 호출, 값 구해오기
-
-    // DTO로 직렬화
-
+    PathFindResponse response = TmapToPathMapper.toPathFindResponseDto(
+            tmapApiClient.tmapApiPathFind(
+                    TmapToPathMapper.toTmapPathFindRequestDto(pathFindRequest)));
     // 반환
-    return null;
+    return response;
   }
 
   /**
@@ -42,7 +50,10 @@ public class PathServiceImpl implements PathService {
     // find
     PathAddressResponse address = KakaoAddressMapper.toDomain(kakaoApiClient.transferToAddress(lng, lat));
 
-    // TODO : 시도 및 시군구 매칭 검증
+    // TODO : 시도, 시군구의 매칭 검증
+
+    log.info(ProvisionCity.findByName(address.region1DepthName()).getName());
+    log.info(ProvisionCity.findByName(address.region2DepthName()).getName());
 
     // check is service area
     if (ProvisionCity.findByName(address.region1DepthName()).getIsAble()
