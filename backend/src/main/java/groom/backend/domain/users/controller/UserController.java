f1f248281624fa2fb008ad2a2cbd78fdfb27ba1f
mskim <msung98@naver.com>
2025-12-03 17:08:44 +0900

    feat(security) : contorller에서 userId, role가져오는 객체 구현

---
+++ backend/src/main/java/groom/backend/domain/users/controller/UserController.java
@@ -0,0 +1,25 @@
+package groom.backend.domain.users.controller;
+
+import groom.backend.common.security.AuthUser;
+import groom.backend.domain.users.entity.Role;
+import groom.backend.domain.users.entity.User;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+
+    @GetMapping("/")
+    public List<User> findAll(
+            @AuthenticationPrincipal AuthUser user
+    ) {
+        UUID userId = user.userId();
+        Role role = user.role();
+        return null;
+    }
+}
