556617433cba00147272a6714407f37cc64b0052
mskim <msung98@naver.com>
2025-12-05 11:42:57 +0900

    feat(notification) : entity 정의

--- backend/src/main/java/groom/backend/domain/location/repository/impl/LocationRespositoryImpl.java
+++
@@ -1,6 +0,0 @@
-package groom.backend.domain.location.repository.impl;
-
-import groom.backend.domain.location.repository.spec.LocationRepository;
-
-public class LocationRespositoryImpl implements LocationRepository {
-}
