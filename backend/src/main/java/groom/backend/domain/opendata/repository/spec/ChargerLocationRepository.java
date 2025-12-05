5f02ab41650c149914cb9a1e526f922495659ed8
JunwonLee <jununida@gmail.com>
2025-12-05 11:45:48 +0900

    feature(charger): 충전소 조회 기능 repository 추가

--- backend/src/main/java/groom/backend/domain/opendata/repository/spec/ChargerLocationRepository.java
+++ backend/src/main/java/groom/backend/domain/opendata/repository/spec/ChargerLocationRepository.java
@@ -1,5 +1,8 @@
 package groom.backend.domain.opendata.repository.spec;
 
+import groom.backend.domain.opendata.dto.request.NearbyRequest;
+import groom.backend.domain.opendata.dto.request.ViewportRequest;
+import groom.backend.domain.opendata.dto.response.ChargerLocationResponse;
 import groom.backend.domain.opendata.entity.ChargerLocation;
 
 import java.util.List;
@@ -9,4 +12,22 @@ public interface ChargerLocationRepository {
     void deleteAll();
 
     void batchInsert(List<ChargerLocation> items);
+
+    /**
+     * Viewport 기반 조회 (사각형 영역)
+     */
+    List<ChargerLocationResponse> findByLatBetweenAndLngBetween(ViewportRequest viewportRequest);
+
+    /**
+     * 반경 기반 조회 (Haversine 공식)
+     *
+     * Haversine 공식:
+     * distance = 2 * R * asin(sqrt(sin²((lat2-lat1)/2) + cos(lat1)*cos(lat2)*sin²((lng2-lng1)/2)))
+     * R = 지구 반지름 (6371 km)
+     */
+    List<ChargerLocationResponse> findNearbyChargers(NearbyRequest nearbyRequest);
+
+    List<ChargerLocationResponse> findAll();
+
+    ChargerLocationResponse findById(Long id);
 }
