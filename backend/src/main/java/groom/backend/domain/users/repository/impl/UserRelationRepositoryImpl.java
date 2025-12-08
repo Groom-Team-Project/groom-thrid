d1d6fe0bed6cf36713b52d04e81998ab18738374
mskim <msung98@naver.com>
2025-12-08 14:22:19 +0900

    feat(sse) : relation repositoryImpl 구현

---
+++ backend/src/main/java/groom/backend/domain/users/repository/impl/UserRelationRepositoryImpl.java
@@ -0,0 +1,39 @@
+package groom.backend.domain.users.repository.impl;
+
+import groom.backend.domain.users.entity.UserRelation;
+import groom.backend.domain.users.repository.jpa.JpaUserRelationRepository;
+import groom.backend.domain.users.repository.spec.UserRelationRepository;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * UserRelation Repository 구현체
+ */
+@Repository
+@RequiredArgsConstructor
+public class UserRelationRepositoryImpl implements UserRelationRepository {
+
+    private final JpaUserRelationRepository jpaUserRelationRepository;
+
+    @Override
+    public Optional<UserRelation> findByUserId(UUID userId) {
+        return jpaUserRelationRepository.findByUserIdWithProtector(userId);
+    }
+
+    @Override
+    public UserRelation save(UserRelation userRelation) {
+        return jpaUserRelationRepository.save(userRelation);
+    }
+
+    @Override
+    public void delete(UserRelation userRelation) {
+        jpaUserRelationRepository.delete(userRelation);
+    }
+
+    @Override
+    public boolean existsByUserId(UUID userId) {
+        return jpaUserRelationRepository.existsByUserId(userId);
+    }
+}
