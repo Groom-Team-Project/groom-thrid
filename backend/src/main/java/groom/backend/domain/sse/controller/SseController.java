a804955f97fb62e1ac9751fdf728886f48dcc87d
mskim <msung98@naver.com>
2025-12-08 10:38:13 +0900

    feat(sse) : controller 진입점 추가

--- backend/src/main/java/groom/backend/domain/sse/controller/SseController.java
+++ backend/src/main/java/groom/backend/domain/sse/controller/SseController.java
@@ -4,9 +4,11 @@ import groom.backend.common.security.AuthUser;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/v1/sse")
@@ -15,12 +17,18 @@ public class SseController {
 
     private final SseServiceImpl sseService;
 
-    @PostMapping("/connect")
-    public void connect(@AuthenticationPrincipal AuthUser user) {
+    @GetMapping("/connect")
+    public SseEmitter connect(@AuthenticationPrincipal AuthUser user) {
 
         UUID userId = user.userId();
 
-        sseService.connect(userId);
+        return sseService.connect(userId);
     }
 
+    @PostMapping("/disconnect")
+    public void disconnect(@AuthenticationPrincipal AuthUser user) {
+        UUID userId = user.userId();
+
+        sseService.disconnect(userId);
+    }
 }
