540923c14c449b3817a5ed5faeb421622140ce33
mskim <msung98@naver.com>
2025-12-08 16:10:03 +0900

    feat(sse) : 위치정보 컨트롤러 정의

---
+++ backend/src/main/java/groom/backend/domain/location/controller/LocationController.java
@@ -0,0 +1,40 @@
+package groom.backend.domain.location.controller;
+
+import groom.backend.common.security.AuthUser;
+import groom.backend.domain.location.dto.request.LocationUpdateRequest;
+import groom.backend.domain.location.service.impl.LocationServiceImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/locations")
+@RequiredArgsConstructor
+@Slf4j
+public class LocationController {
+
+    private final LocationServiceImpl locationService;
+
+    // 위치 정보 업데이트
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateLocation(
+            @AuthenticationPrincipal AuthUser user,
+            @Valid @RequestBody LocationUpdateRequest request) {
+
+        log.info("위치 정보 업데이트 요청: userId={}, lat={}, lng={}",
+                user.userId(), request.getLatitude(), request.getLongitude());
+
+        locationService.updateLocation(
+                user.userId(),
+                request
+        );
+    }
+}
