80a594f0029f18b9d483e67bcd718762ca311fe7
mskim <msung98@naver.com>
2025-12-08 10:38:29 +0900

    feat(sse) : service spec 정의

---
+++ backend/src/main/java/groom/backend/domain/sse/service/spec/SseService.java
@@ -0,0 +1,15 @@
+package groom.backend.domain.sse.service.spec;
+
+import java.util.UUID;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface SseService {
+
+    SseEmitter connect(UUID userId);
+
+    void disconnect(UUID userId);
+
+    boolean isConnect(UUID userId);
+
+    void send(UUID userId, Object data);
+}
